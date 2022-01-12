f2ebfd1fea544e3ba6d6a1c55289ab954bedc0cc
Floris272 <florispuijk@outlook.com>
2022-01-12 11:55:11 +0100

    utils fixes for updated keys and timestamps

--- src/bluecurrent_api/utils.py
+++ src/bluecurrent_api/utils.py
@@ -3,12 +3,17 @@ from datetime import datetime
 
 def calculate_usage_from_phases(phases):
     phases = [p for p in phases if p]
-    return round(sum(phases) / len(phases), 1)
+    if len(phases):
+        return round(sum(phases) / len(phases), 1)
+    else:
+        return 0
 
 
 def create_datetime(timestamp):
+    if timestamp == "":
+        return None
     timestamp += "+02:00"
-    return datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S%z")
+    return datetime.strptime(timestamp, "%Y%m%d %H:%M:%S%z")
 
 
 def get_vehicle_status(vehicle_status_key):
@@ -16,43 +21,43 @@ def get_vehicle_status(vehicle_status_key):
         "A": "standby",
         "B": "vehicle detected",
         "C": "ready",
-        "D":  "with ventialtion",
-        "E":  "no power",
-        "F":  "error",
+        "D": "with ventialtion",
+        "E": "no power",
+        "F": "error",
     }
 
     return statuses[vehicle_status_key]
 
 
 def handle_status(message):
-    v1 = message["data"]["actual_v1"]
-    v2 = message["data"]["actual_v2"]
-    v3 = message["data"]["actual_v3"]
+    v1 = message["data"]["ch_actual_v1"]
+    v2 = message["data"]["ch_actual_v2"]
+    v3 = message["data"]["ch_actual_v3"]
 
     v_total = calculate_usage_from_phases((v1, v2, v3))
-    message["data"]["total_voltage"] = v_total
+    message["data"]["ch_total_voltage"] = v_total
 
-    c1 = message["data"]["actual_p1"]
-    c2 = message["data"]["actual_p2"]
-    c3 = message["data"]["actual_p3"]
+    c1 = message["data"]["ch_actual_p1"]
+    c2 = message["data"]["ch_actual_p2"]
+    c3 = message["data"]["ch_actual_p3"]
 
     c_total = calculate_usage_from_phases((c1, c2, c3))
-    message["data"]["total_current"] = c_total
+    message["data"]["ch_total_current"] = c_total
 
     vehicle_status_key = message["data"]["vehicle_status"]
     message["data"]["vehicle_status"] = get_vehicle_status(vehicle_status_key)
 
-    start_session = message["data"]["start_session"]
-    new_start_session = create_datetime(start_session)
+    start_datetime = message["data"]["start_datetime"]
+    new_start_datetime = create_datetime(start_datetime)
 
-    stop_session = message["data"]["stop_session"]
-    new_stop_session = create_datetime(stop_session)
+    stop_datetime = message["data"]["stop_datetime"]
+    new_stop_datetime = create_datetime(stop_datetime)
 
-    message["data"]["start_session"] = new_start_session
-    message["data"]["stop_session"] = new_stop_session
+    message["data"]["start_datetime"] = new_start_datetime
+    message["data"]["stop_datetime"] = new_stop_datetime
 
-    offline_since = message["data"]["offline_since"]
-    message["data"]["offline_since"] = create_datetime(offline_since)
+    offline_since = message["data"]["ch_offline_since"]
+    message["data"]["ch_offline_since"] = create_datetime(offline_since)
 
 
 def handle_grid(message):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,6 +6,9 @@ def test_calculate_total_from_phases():
     total = calculate_usage_from_phases((10, 10, 10))
     assert total == 10
 
+    total = calculate_usage_from_phases((0, 0, 0))
+    assert total == 0
+
     total = calculate_usage_from_phases((10, None, 20))
     assert total == 15
 
@@ -35,40 +38,43 @@ def test_get_vehicle_status():
 
 
 def test_create_datetime():
-    assert create_datetime("2021-11-18T14:12:23") == datetime(
-        2021, 11, 18, 14, 12, 23, tzinfo=timezone(timedelta(seconds=7200))
+
+    assert create_datetime("20010101 00:00:00") == datetime(
+        2001, 1, 1, 0, 0, 0, tzinfo=timezone(timedelta(seconds=7200))
     )
+
+    assert create_datetime("") == None
 
 
 def test_handle_status():
     message = {
         "data": {
-            'actual_v1': 12,
-            'actual_v2': 14,
-            'actual_v3': 15,
-            'actual_p1': 12,
-            'actual_p2': 10,
-            'actual_p3': 15,
-            'activity': "charging",
-            'start_session': "2021-11-18T14:12:23",
-            'stop_session': "2021-11-18T14:32:23",
-            'offline_since': "2021-11-18T14:32:23",
+            'ch_actual_v1': 12,
+            'ch_actual_v2': 14,
+            'ch_actual_v3': 15,
+            'ch_actual_p1': 12,
+            'ch_actual_p2': 10,
+            'ch_actual_p3': 15,
+            'ch_activity': "charging",
+            'start_datetime': "20211118 14:12:23",
+            'stop_datetime': "20211118 14:32:23",
+            'ch_offline_since': "20211118 14:32:23",
             'total_cost': 10.52,
             'vehicle_status': "A",
-            'actual_kwh': 10,
+            'ch_actual_kwh': 10,
             'evse_id': "101",
         }
     }
 
     handle_status(message)
 
-    assert message["data"]["total_voltage"] == 13.7
-    assert message["data"]["total_current"] == 12.3
-    assert message["data"]["start_session"] == datetime(
+    assert message["data"]["ch_total_voltage"] == 13.7
+    assert message["data"]["ch_total_current"] == 12.3
+    assert message["data"]["start_datetime"] == datetime(
         2021, 11, 18, 14, 12, 23, tzinfo=timezone(timedelta(seconds=7200)))
-    assert message["data"]["stop_session"] == datetime(
+    assert message["data"]["stop_datetime"] == datetime(
         2021, 11, 18, 14, 32, 23, tzinfo=timezone(timedelta(seconds=7200)))
-    assert message["data"]["offline_since"] == datetime(
+    assert message["data"]["ch_offline_since"] == datetime(
         2021, 11, 18, 14, 32, 23, tzinfo=timezone(timedelta(seconds=7200)))
     assert message["data"]["vehicle_status"] == "standby"
 
