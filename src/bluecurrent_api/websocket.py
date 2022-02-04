404aeef58506ebf13c9bebd048f8b25d14ca65e2
Floris272 <florispuijk@outlook.com>
2022-02-04 11:49:20 +0100

    added docstrings + fixed disconnect bug

--- src/bluecurrent_api/websocket.py
+++ src/bluecurrent_api/websocket.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+from typing import Callable
 import websockets
 import ssl
 from asyncio.exceptions import TimeoutError
@@ -19,7 +20,8 @@ class Websocket:
     def __init__(self):
         pass
 
-    async def validate_api_token(self, api_token):
+    async def validate_api_token(self, api_token: str):
+        """Validate an api token."""
         await self._connect()
         await self._send({"command": "VALIDATE_API_TOKEN", "token": api_token})
         res = await self._recv()
@@ -30,6 +32,7 @@ class Websocket:
         return True
 
     async def get_charge_cards(self):
+        """Get the charge cards"""
         if not self.auth_token:
             raise WebsocketError("token not set")
         await self._connect()
@@ -41,17 +44,20 @@ class Websocket:
             raise NoCardsFound()
         return cards
 
-    def set_receiver(self, receiver):
+    def set_receiver(self, receiver: Callable):
+        """Set a receiver."""
         self.receiver = receiver
         self.receiver_is_coroutine = asyncio.iscoroutinefunction(receiver)
 
-    async def connect(self, api_token):
+    async def connect(self, api_token: str):
+        """Validate token and connect to the websocket."""
         if self._has_connection:
             raise WebsocketError("Connection already started.")
         await self.validate_api_token(api_token)
         await self._connect()
 
     async def _connect(self):
+        """Connect to the websocket."""
         # Needed for wss.
         def get_ssl():
             ssl_context = ssl.create_default_context()
@@ -65,7 +71,8 @@ class Websocket:
         except (ConnectionRefusedError, TimeoutError) as err:
             raise WebsocketError("Cannot connect to the websocket.", err)
 
-    async def send_request(self, request):
+    async def send_request(self, request: dict):
+        """Add authorization and send request."""
         if not self.receiver:
             raise WebsocketError("receiver method not set")
         if not self.auth_token:
@@ -75,22 +82,26 @@ class Websocket:
         await self._send(request)
 
     async def loop(self):
+        """Loop the message_handler."""
         if not self.receiver:
             raise WebsocketError("receiver method not set")
 
         while True:
-            await self._message_handler()
+            stop = await self._message_handler()
+            if stop:
+                break
 
     async def _message_handler(self):
+        """Wait for a message and give it to the receiver."""
         message: dict = await self._recv()
         if not message:
-            return
+            return True
 
         object_name = message.get("object")
         error_code = message.get("error")
 
         if error_code == 0:
-            raise WebsocketError("Send unknown command")
+            raise WebsocketError("Unknown command")
         elif error_code == 1 or error_code == 2:
             raise InvalidToken
         elif error_code == 9:
@@ -108,18 +119,20 @@ class Websocket:
         else:
             self.receiver(message)
 
-    async def _send(self, data):
-        self.check_connection()
+    async def _send(self, data: dict):
+        """Send data to the websocket."""
+        self._check_connection()
         try:
-            data = json.dumps(data)
-            await self._connection.send(data)
+            data_str = json.dumps(data)
+            await self._connection.send(data_str)
         except ConnectionClosed:
             if self._has_connection:
                 self._has_connection = False
                 raise WebsocketError("connection was closed.")
 
     async def _recv(self):
-        self.check_connection()
+        """Receive data from de websocket."""
+        self._check_connection()
         try:
             data = await self._connection.recv()
             return json.loads(data)
@@ -129,12 +142,15 @@ class Websocket:
                 raise WebsocketError("connection was closed.")
 
     async def disconnect(self):
-        self.check_connection()
+        """Disconnect from de websocket"""
+        self._check_connection()
         if not self._has_connection:
+
             raise WebsocketError("Connection is already closed.")
         self._has_connection = False
         await self._connection.close()
 
-    def check_connection(self):
+    def _check_connection(self):
+        """check if the connection still exists."""
         if not self._connection:
             raise WebsocketError("No connection with the api.")
